8ec2937f960d56e3dafff170338bf82c474c4455
Mig3L_0n <ldglmapryt21@gmai.com>
2023-07-26 10:13:57 -0500

    Agregado vista de fecha y hora en el log

--- Mig3L_0n.py
+++ Mig3L_0n.py
@@ -1,13 +1,14 @@
 from twitchio.ext import commands
+from datetime import datetime
 import time
 from os import system
 system("title KUKORO - MAZMORRA")
 
 # REEMPLAZAR POR TUS DATOS
-Canales = ['CANAL']
-Administradores = ['ADMINISTRADOR'] 
-Comando = 'COMANDO'
-TokenTwitch = 'TOKEN'
+Canales = ['CANAL(ES)']
+Administradores = ['ADMINISTRADOR(ES)']
+Comando = 'TU_COMANDO'
+TokenTwitch = 'TOKEN_DEL_BOT'
 
 class Bot(commands.Bot):
     def __init__(self):         
@@ -17,17 +18,19 @@ class Bot(commands.Bot):
         print(f'\x1b[3;35m --> {self.nick.capitalize()}\x1b[3;32m conectado a {Canales}\x1b[0;m')  
         print(f'\x1b[3;35m --> Administradores: \x1b[3;37m{Administradores}\x1b[0;m')  
         print(f'\x1b[3;33m --> {Comando}\x1b[3;34m para invocarlo') 
-
+        
     async def event_message(self, ctx):
         if ctx.echo:
             return
         Canal = (f'{ctx.channel}').replace('<Channel name: ','').replace('>','')
         Mensaje = f'{ctx.content}'
         if(Administradores.__contains__(ctx.author.name)):
-            if(Mensaje.lower() == Comando.lower()):  
-                await ctx.channel.send('!kukoro')
+            if(Mensaje.lower() == Comando.lower()):                             
+                await ctx.channel.send('!KuKoRo')
                 time.sleep(2)
-                await ctx.channel.send('!getinfo')  
-                print(f'\x1b[3;36m --> Invocado por {ctx.author.name} en {Canal}\x1b[0;m')                            
+                await ctx.channel.send('!GeTiNfO')
+                now = datetime.now()                
+                fechahora = now.strftime("%d/%m/%Y %H:%M:%S") 
+                print(f'\x1b[3;36m --> [{fechahora}] Invocado por {ctx.author.name} en {Canal}\x1b[0;m')                            
 bot = Bot()
 bot.run()
